c42572af72fd21a798c7195032a84b12fac343c7
gabrielcjm <kevencript@gmail.com>
2022-05-01 13:07:59 -0300

    feat: Criando função utilitária pinJSONtoIPFS() para converter objetos JSON em IPFS (irá gerar uma URL)

--- src/utils/pinata.js
+++ src/utils/pinata.js
@@ -6,7 +6,32 @@ const bearer = process.env.REACT_APP_BEARER_KEY;
 const axios = require("axios");
 
 // Função para converter JSON em IPFS
-export const pinJSONToIPFS = async (JSONBody) => {};
+export const pinJSONToIPFS = async (JSONBody) => {
+  const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+  // Realizando uma requisição para a API do piñata para
+  // transformar um objeto JSON em um IPFS
+  return axios
+    .post(url, JSONBody, {
+      headers: {
+        pinata_api_key: key,
+        pinata_secret_api_key: secret,
+      },
+    })
+    .then(function (response) {
+      return {
+        success: true,
+        pinataUrl:
+          "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash,
+      };
+    })
+    .catch(function (error) {
+      console.log(error);
+      return {
+        success: false,
+        message: error.message,
+      };
+    });
+};
 
 // Função para transformar a imagem da Arte do Projeto em IPFS
 export const pinIMAGEtoIPFS = async (data) => {
