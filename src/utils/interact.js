49d17915bf37c97dec591aa387ced14a6ae67fa4
gabrielcjm <kevencript@gmail.com>
2022-05-01 12:51:21 -0300

    feat: criando função utilitária connectWallet() para conectar a carteira Metamask do usuário na nossa aplicação

--- src/utils/interact.js
+++ src/utils/interact.js
@@ -10,7 +10,35 @@ const web3 = createAlchemyWeb3(alchemyKey);
 export const loadContract = async () => {};
 
 // Função para abrir a opção de conectar carteira na Metamask
-export const connectWallet = async () => {};
+export const connectWallet = async () => {
+  if (window.ethereum) {
+    // Se a Metamask estiver instalada no navegador
+    try {
+      // Inicia o processo de habilitação de uma carteira da Metamask para a nossa aplicação
+      const address = await window.ethereum.enable();
+      const obj = {
+        // Objeto contendo o endereço da carteira do usuário e os status da operação
+        connectedStatus: true,
+        status: "",
+        address: address,
+      };
+      return obj;
+    } catch (error) {
+      // Caso haja algum erro
+      return {
+        connectedStatus: false,
+        status:
+          "🦊 Conecte sua carteira na Metamask no botão no topo à direita",
+      };
+    }
+  } else {
+    return {
+      connectedStatus: false,
+      status:
+        "🦊  Por favor, instale a Metamask no seu navegador: https://metamask.io/download.html",
+    };
+  }
+};
 
 // Função para realizar o Mint de um NFTT a partir dos parâmetros
 export const mintNFT = async (artist, artwork, name) => {};
