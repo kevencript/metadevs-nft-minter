7f97c71d45c306d053242dcd039b7a1dc9821ac9
gabrielcjm <kevencript@gmail.com>
2022-05-01 12:40:04 -0300

    feat: Criando o SmartContract principal com OpenZeppelin

--- src/contracts/MetadevsContract.sol
+++ src/contracts/MetadevsContract.sol
@@ -6,5 +6,21 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MetadevsContract is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     constructor() ERC721("MetadevsToken", "MDTKN") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
 }
