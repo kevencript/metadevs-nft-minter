5a14b3979728639401e56ffb0e013505d642860e
gabrielcjm <kevencript@gmail.com>
2022-05-01 12:58:55 -0300

    feat: Criando função utilitária pinIMAGEtoIPFS() para converter imagens em IPFS (gera uma URL) por meio da API do Piñata

--- src/utils/pinata.js
+++ src/utils/pinata.js
@@ -9,4 +9,37 @@ const axios = require("axios");
 export const pinJSONToIPFS = async (JSONBody) => {};
 
 // Função para transformar a imagem da Arte do Projeto em IPFS
-export const pinIMAGEtoIPFS = async (imageBody) => {};
+export const pinIMAGEtoIPFS = async (data) => {
+  const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+  // Realizando uma requisição para a API do piñata para
+  // transformar uma imagem em um IPFS
+  return (
+    axios
+      .post(url, data, {
+        maxBodyLength: "Infinity",
+        headers: {
+          // Obtendo variáveis de ambiente da nossa conta do piñata
+          pinata_api_key: key,
+          pinata_secret_api_key: secret,
+        },
+      })
+      // Depois de subir a imagem e obter uma resposta, iremos definir
+      // algumas variáveis de stado da aplicação definindo o link/hash do
+      // IPFS gerado
+      .then(function (response) {
+        console.log(response);
+        return {
+          success: true,
+          pinataUrl:
+            "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash,
+        };
+      })
+      .catch(function (error) {
+        // Caso algo dê errado, iremos tratar o erro aqui
+        return {
+          success: false,
+          error: "Erro ao realizar upload da Arte do Trabalho",
+        };
+      })
+  );
+};
